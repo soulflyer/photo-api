c965175c812516c2e42e40545dcc25f6f9878e51
iain <iain@soulflyer.co.uk>
2022-07-03 08:08:09 +0700

    modified start-photo-api.sh

--- bin/start-photo-api.sh
+++ bin/start-photo-api.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+jarfile=/Users/iain/Code/Clojure/Luminus/photo-api/target/uberjar/photo-api.jar
+
 echo "Starting photo api, look in /tmp/photo-api.log for messages."
-java -Ddatabase-url="mongodb://127.0.0.1/photos" -jar /Users/iain/Code/Clojure/Luminus/photo-api/target/uberjar/photo-api.jar &>/tmp/photo-api.log &
+java -Ddatabase-url="mongodb://127.0.0.1/photos" -jar $jarfile &>/tmp/photo-api.log &
